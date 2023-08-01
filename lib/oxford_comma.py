012abea420aec6761d0ee6a41b2f55b2ecdb2886
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-01 16:11:40 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,6 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1 or len(items) == 2:
+        return " and ".join(items)
+    else:
+        items[-1] = f'and {items[-1]}'
+        return ", ".join(items)
